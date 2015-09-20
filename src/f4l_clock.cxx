cfa291182f31cbf48c835454ec2ab22b8e8dc579
Philipp Janda <siffiejoe@gmx.net>
2015-09-20 09:14:43 +0200

    Add binding to clock (output) widgets.

---
+++ src/f4l_clock.cxx
@@ -0,0 +1,156 @@
+#include "fltk4lua.hxx"
+#include "f4l_widget.hxx"
+#include <FL/Fl_Clock.H>
+#include <FL/Fl_Round_Clock.H>
+#include <cstring>
+#include <climits>
+
+namespace {
+
+  inline Fl_Clock_Output* check_clock( lua_State* L, int idx ) {
+    void* p = moon_checkobject( L, idx, F4L_CLOCK_OUTPUT_NAME );
+    return static_cast< Fl_Clock_Output* >( p );
+  }
+
+  int clock_index_( lua_State* L, Fl_Clock_Output* c,
+                    char const* key, size_t n ) {
+    switch( n ) {
+      case 4:
+        if( F4L_MEMCMP( key, "hour", 4 ) == 0 ) {
+          lua_pushinteger( L, c->hour() );
+          return 1;
+        }
+        break;
+      case 5:
+        if( F4L_MEMCMP( key, "value", 5 ) == 0 ) {
+          if( sizeof( lua_Integer ) * CHAR_BIT > 32 )
+            lua_pushinteger( L, c->value() );
+          else
+            lua_pushnumber( L, c->value() );
+          return 1;
+        }
+        break;
+      case 6:
+        if( F4L_MEMCMP( key, "minute", 6 ) == 0 ) {
+          lua_pushinteger( L, c->minute() );
+          return 1;
+        } else if( F4L_MEMCMP( key, "second", 6 ) == 0 ) {
+          lua_pushinteger( L, c->second() );
+          return 1;
+        }
+        break;
+    }
+    return 0;
+  }
+
+  int clock_newindex_( lua_State* L, Fl_Clock_Output* c,
+                       char const* key, size_t n ) {
+    switch( n ) {
+      case 5:
+        if( F4L_MEMCMP( key, "value", 5 ) == 0 ) {
+          if( sizeof( lua_Integer ) * CHAR_BIT > 32 )
+            c->value( moon_checkint( L, 3, 0, ULONG_MAX ) );
+          else
+            c->value( static_cast< ulong >( luaL_checknumber( L, 3 ) ) );
+          return 1;
+        }
+        break;
+    }
+    return 0;
+  }
+
+  int clock_index( lua_State* L ) {
+    Fl_Clock_Output* c = check_clock( L, 1 );
+    size_t n = 0;
+    char const* key = luaL_checklstring( L, 2, &n );
+    F4L_TRY {
+      if( !clock_index_( L, c, key, n ) &&
+          !f4l_widget_index_( L, c, key, n ) )
+        lua_pushnil( L );
+    } F4L_CATCH( L );
+    return 1;
+  }
+
+  int clock_newindex( lua_State* L ) {
+    Fl_Clock_Output* c = check_clock( L, 1 );
+    size_t n = 0;
+    char const* key = luaL_checklstring( L, 2, &n );
+    F4L_TRY {
+      (void)(clock_newindex_( L, c, key, n ) ||
+             f4l_widget_newindex_( L, c, key, n ));
+    } F4L_CATCH( L );
+    return 0;
+  }
+
+  int new_clock_output( lua_State* L ) {
+    F4L_TRY {
+      f4l_new_widget< Fl_Clock_Output >( L, F4L_CLOCK_OUTPUT_NAME );
+    } F4L_CATCH( L );
+    return 1;
+  }
+
+  int new_clock( lua_State* L ) {
+    F4L_TRY {
+      f4l_new_widget< Fl_Clock >( L, F4L_CLOCK_NAME );
+    } F4L_CATCH( L );
+    return 1;
+  }
+
+  int new_round_clock( lua_State* L ) {
+    F4L_TRY {
+      f4l_new_widget< Fl_Round_Clock >( L, F4L_ROUND_CLOCK_NAME );
+    } F4L_CATCH( L );
+    return 1;
+  }
+
+
+  int clock_setvalue( lua_State* L ) {
+    Fl_Clock_Output* c = check_clock( L, 1 );
+    int h = moon_checkint( L, 2, 0, 23 );
+    int m = moon_checkint( L, 3, 0, 59 );
+    int s = moon_checkint( L, 4, 0, 60 );
+    F4L_TRY {
+      c->value( h, m, s );
+    } F4L_CATCH( L );
+    return 0;
+  }
+
+} // anonymous namespace
+
+
+MOON_LOCAL void f4l_clock_setup( lua_State* L ) {
+  luaL_Reg const functions[] = {
+    { "Clock_Output", new_clock_output },
+    { "Clock", new_clock },
+    { "Round_Clock", new_round_clock },
+    { NULL, NULL }
+  };
+  luaL_Reg const methods[] = {
+    F4L_WIDGET_METHODS,
+    { "setvalue", clock_setvalue },
+    { "__index", clock_index },
+    { "__newindex", clock_newindex },
+    { NULL, NULL }
+  };
+
+  moon_defobject( L, F4L_CLOCK_OUTPUT_NAME, 0, methods, 0 );
+  moon_defcast( L, F4L_CLOCK_OUTPUT_NAME, F4L_WIDGET_NAME,
+                f4l_cast< Fl_Clock_Output, Fl_Widget > );
+
+  moon_defobject( L, F4L_CLOCK_NAME, 0, methods, 0 );
+  moon_defcast( L, F4L_CLOCK_NAME, F4L_CLOCK_OUTPUT_NAME,
+                f4l_cast< Fl_Clock, Fl_Clock_Output > );
+  moon_defcast( L, F4L_CLOCK_NAME, F4L_WIDGET_NAME,
+                f4l_cast< Fl_Clock, Fl_Widget > );
+
+  moon_defobject( L, F4L_ROUND_CLOCK_NAME, 0, methods, 0 );
+  moon_defcast( L, F4L_ROUND_CLOCK_NAME, F4L_CLOCK_NAME,
+                f4l_cast< Fl_Round_Clock, Fl_Clock > );
+  moon_defcast( L, F4L_ROUND_CLOCK_NAME, F4L_CLOCK_OUTPUT_NAME,
+                f4l_cast< Fl_Round_Clock, Fl_Clock_Output > );
+  moon_defcast( L, F4L_ROUND_CLOCK_NAME, F4L_WIDGET_NAME,
+                f4l_cast< Fl_Round_Clock, Fl_Widget > );
+
+  luaL_setfuncs( L, functions, 0 );
+}
+
--- src/fltk4lua.cxx
+++ src/fltk4lua.cxx
@@ -198,6 +198,7 @@ MOON_LOCAL void f4l_group_setup( lua_State* L );
 MOON_LOCAL void f4l_window_setup( lua_State* L );
 MOON_LOCAL void f4l_box_setup( lua_State* L );
 MOON_LOCAL void f4l_button_setup( lua_State* L );
+MOON_LOCAL void f4l_clock_setup( lua_State* L );
 MOON_LOCAL void f4l_adjuster_setup( lua_State* L );
 MOON_LOCAL void f4l_counter_setup( lua_State* L );
 MOON_LOCAL void f4l_dial_setup( lua_State* L );
@@ -237,6 +238,7 @@ F4L_API int luaopen_fltk4lua( lua_State* L ) {
   f4l_window_setup( L );
   f4l_box_setup( L );
   f4l_button_setup( L );
+  f4l_clock_setup( L );
   f4l_adjuster_setup( L );
   f4l_counter_setup( L );
   f4l_dial_setup( L );
