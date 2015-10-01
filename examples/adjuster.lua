f58761bc688fe681bdc19c4501b6854eebb40710
Philipp Janda <siffiejoe@gmx.net>
2015-10-01 08:47:09 +0200

    Cosmetic fixes to adjuster.lua example.

--- examples/adjuster.lua
+++ examples/adjuster.lua
@@ -2,10 +2,10 @@
 
 local fl = require( "fltk4lua" )
 local window = fl.Window( 320, 100, arg[ 0 ] )
-local b1 = fl.Box( 20, 30, 80, 25 )
+local b1 = fl.Box( 20, 35, 80, 25 )
 b1.box = "FL_DOWN_BOX"
 b1.color = fl.WHITE
-local a1 = fl.Adjuster( 20+80, 30, 3*25, 25 )
+local a1 = fl.Adjuster( 20+80, 35, 3*25, 25 )
 a1.user_data = b1
 function a1:callback( v )
   v.label = self:format()
@@ -13,7 +13,7 @@ function a1:callback( v )
 end
 a1:callback( b1 )
 
-local b2 = fl.Box( 20+80+4*25, 30, 80, 25 )
+local b2 = fl.Box( 20+80+4*25, 35, 80, 25 )
 b2.box = "FL_DOWN_BOX"
 b2.color = fl.WHITE
 local a2 = fl.Adjuster( b2.x+b2.w, 10, 25, 3*25 )
@@ -21,7 +21,7 @@ a2.user_data = b2
 a2.callback = a1.callback
 a2:callback( b2 )
 
-window.resizable = window
+--window.resizable = window -- looks bad in a tiling window manager
 window:end_group()
 window:show( arg )
 fl.run()
