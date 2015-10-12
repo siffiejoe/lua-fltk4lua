52b01acf8307e0eb9adf556f871ff5c9fb7c8fc1
Philipp Janda <siffiejoe@gmx.net>
2015-10-12 15:47:30 +0200

    Simplify (and fix) chart.lua example script.

--- examples/chart.lua
+++ examples/chart.lua
@@ -1,7 +1,9 @@
 #!/usr/bin/env lua
 
+math.randomseed( os.time() )
 local fl = require( "fltk4lua" )
 local window = fl.Window( 500, 320 )
+
 local chart_types = {
   "FL_BAR_CHART", "FL_FILLED_CHART", "FL_HORBAR_CHART",
   "FL_LINE_CHART", "FL_PIE_CHART", "FL_SPECIALPIE_CHART",
@@ -18,8 +20,7 @@ function choice:callback( chart )
 end
 local chart1 = fl.Chart{
   250, 10, 230, 150, "Chart 1",
-  type=chart_types[ 1 ], color=fl.WHITE,
-  autosize=true, box="FL_ROUNDED_BOX",
+  type=chart_types[ 1 ], box="FL_ROUNDED_BOX", color=fl.WHITE,
 }
 chart1:add( 10, "item 1", fl.BLUE )
 chart1:add( 25, "item 2", fl.RED )
@@ -28,30 +29,18 @@ chart1:add( 5, "item 4", fl.CYAN )
 chart1:add( 20, "item 5", fl.YELLOW )
 choice.user_data = chart1
 
-math.randomseed( os.time() )
-local values = {}
-for i = 1, 10 do
-  values[ i ] = math.random( 10, 100 )
-end
-
 local retb = fl.Return_Button( 50, 140, 180, 20, "Step" )
 function retb:callback( chart )
-  table.remove( values, 1 )
-  values[ #values+1 ] = math.random( 10, 100 )
-  chart:clear()
-  for _,v in ipairs( values ) do
-    chart:add( v, nil, fl.BLUE )
-  end
-  chart:redraw()
+  chart:add( math.random( 10, 100 ), nil, fl.BLUE )
 end
 local chart2 = fl.Chart{
   10, 180, 480, 110, "Chart 2",
-  type="FL_LINE_CHART", color=fl.WHITE,
-  box="FL_ROUNDED_BOX",
+  type="FL_FILLED_CHART", box="FL_ROUNDED_BOX", color=fl.WHITE,
+  autosize=false, maxsize=10,
 }
 chart2:bounds( 0, 100 )
-for _,v in ipairs( values ) do
-  chart2:add( v, nil, fl.BLUE )
+for i = 1, 3 do
+  retb:callback( chart2 )
 end
 retb.user_data = chart2
 
