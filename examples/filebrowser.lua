139f6e49b4de99d750d7d209857b5f3e2aebfe0b
Philipp Janda <siffiejoe@gmx.net>
2015-10-17 22:51:09 +0200

    Add example script featuring File_Browser.

---
+++ examples/filebrowser.lua
@@ -0,0 +1,26 @@
+#!/usr/bin/env lua
+
+local dir = "examples"
+local dirsep = package.config:sub( 1, 1 )
+local exe, i = arg[ 0 ], -1
+while arg[ i ] do
+  exe, i = arg[ i ], i - 1
+end
+
+local fl = require( "fltk4lua" )
+local window = fl.Window( 400, 400 )
+local fbrowser = fl.File_Browser( 0, 0, 400, 400, "Select Example:" )
+fbrowser.filter = "*.lua"
+fbrowser.type = "FL_SELECT_BROWSER"
+function fbrowser:callback()
+  local fname = self:text( self.value )
+  print( self.value, fname )
+  if not fname:match( "[/\\]$" ) then
+    print( os.execute( exe.." \""..dir..dirsep..fname.."\"" ) )
+  end
+end
+assert( fbrowser:load( dir, "fl_alphasort" ) )
+window:end_group()
+window:show( arg )
+fl.run()
+
